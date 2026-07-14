28f36ac2f83de7996eb66b098296ea65af27b62e
11-01 <129143795+11-01@users.noreply.github.com>
2026-07-14 00:24:22 -0900

    Delete src directory

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<style>
-body { margin: 0; font-family: 'PingFang SC', 'Microsoft YaHei', sans-serif; background: #fdf6f0; }
-#app { min-height: 100vh; }
-</style>
